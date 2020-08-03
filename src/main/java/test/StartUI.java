9adda0999962aa1c7aa967be142950ccae443e0a
Sekator778 <sekator778@gmail.com>
2020-08-03 10:54:25 +0300

    init through field

--- src/main/java/test/StartUI.java
+++ src/main/java/test/StartUI.java
@@ -1,5 +1,6 @@
 package test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -7,11 +8,8 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class StartUI {
+    @Autowired
     private Store store;
-
-    public StartUI(Store store) {
-        this.store = store;
-    }
 
     public void add(String data) {
         this.store.add(data);

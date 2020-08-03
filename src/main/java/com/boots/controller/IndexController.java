cac593545a503a3522ece0ed2d5f923d3822a8a4
Sekator778 <sekator778@gmail.com>
2020-08-03 20:07:36 +0300

    entity manager delete

--- src/main/java/com/boots/controller/IndexController.java
+++ src/main/java/com/boots/controller/IndexController.java
@@ -3,7 +3,6 @@ package com.boots.controller;
 
 import com.boots.entity.Post;
 import com.boots.service.PostService;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/boots/service/PostService.java
+++ src/main/java/com/boots/service/PostService.java
@@ -3,20 +3,13 @@ package com.boots.service;
 
 import com.boots.entity.Post;
 import com.boots.repository.PostRepository;
-import com.boots.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PostService {
-    @PersistenceContext
-    private EntityManager em;
     @Autowired
     PostRepository repository;
 
@@ -26,13 +19,16 @@ public class PostService {
     public List<Post> getAll() {
         return repository.findAll();
     }
+
     public void addPost(Post post) {
         repository.saveAndFlush(post);
     }
+
     public void update(Post post) {
         repository.deleteById(post.getId());
         repository.saveAndFlush(post);
     }
+
     public void deletePostById(Long id) {
         repository.deleteById(id);
     }

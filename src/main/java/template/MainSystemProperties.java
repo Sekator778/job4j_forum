4a272edb8f8daf8c969ed68f6219da189442cbb9
Sekator778 <sekator778@gmail.com>
2020-08-06 15:15:59 +0300

    jdk

--- src/main/java/template/MainSystemProperties.java
+++ src/main/java/template/MainSystemProperties.java
@@ -1,6 +1,17 @@
-package template;/**
- * 
- */
+package template;
+import java.util.Properties;
 
 public class MainSystemProperties {
+    public static void main(String[] a) {
+        // List all System properties
+        Properties pros = System.getProperties();
+        pros.list(System.out);
+
+        // Get a particular System property given its key
+        // Return the property value or null
+        System.out.println(System.getProperty("java.home"));
+        System.out.println(System.getProperty("java.library.path"));
+        System.out.println(System.getProperty("java.ext.dirs"));
+        System.out.println(System.getProperty("java.class.path"));
+    }
 }

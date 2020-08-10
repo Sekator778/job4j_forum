500b69e1804b0a3293267f7e51a33fe979efd887
Sekator778 <sekator778@gmail.com>
2020-08-10 19:44:34 +0300

    heroku fix bug

--- src/main/resources/db/insert_002.sql
+++ src/main/resources/db/insert_002.sql
@@ -1,5 +1,5 @@
-insert into t_user (id, username, password)
-values (1, 'admin', 'admin');
+insert into t_user (username, password)
+values ('admin', 'admin');
 insert into t_user_roles (user_id, roles_id)
 VALUES (1, 1),
        (1, 2);

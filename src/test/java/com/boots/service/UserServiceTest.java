3888f4480b01ced391d28c35425110f61ef6be9f
Sekator778 <sekator778@gmail.com>
2020-08-13 15:40:29 +0300

    test up

--- src/test/java/com/boots/service/UserServiceTest.java
+++ src/test/java/com/boots/service/UserServiceTest.java
@@ -10,12 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource("classpath:application-test.properties")
 public class UserServiceTest {
     @Autowired
     private UserService userService;
